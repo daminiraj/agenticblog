9cf287fc844ff3f83cffaa319c21f1c16195fb81
Damini.Gupta <damini.gupta@servicestream.com.au>
2026-06-24 23:38:01 +1000

    adding nodes and graphs

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph,START,END
 from src.llms.openailm import OpenAiLLM
-from src.states.blogstate import Blog,BlogState
+from src.states.blogstate import BlogState
 from src.nodes.blog_node import BlogNode
 class GraphBuilder:
     def __init__(self,llm):
@@ -18,18 +18,43 @@ class GraphBuilder:
         self.graph.add_edge("content_generation", END)
 
         return self.graph
-        # self.graph.add_edge("title_creation", "content_generation")
-        # self.graph.add_edge("content_generation", END)
+
+
+    def build_language_graph(self):
+        self.blog_node_obj = BlogNode(self.llm)
+        self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
+        self.graph.add_node("content_generation", self.blog_node_obj.content_generation)
+        self.graph.add_node("hindi_translation", lambda state:self.blog_node_obj.translation({**state,"current_language":"hindi"}))
+        self.graph.add_node("french_translation", lambda state:self.blog_node_obj.translation({**state,"current_language":"french"}))
+        self.graph.add_node("route", self.blog_node_obj.route)
+
+        ## Edges
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+        self.graph.add_conditional_edges("route",self.blog_node_obj.route_decision,
+        {
+            "hindi":"hindi_translation",
+            "french":"french_translation"
+        })
+
+        self.graph.add_edge("hindi_translation", END)
+        self.graph.add_edge("french_translation", END)
 
         return self.graph
 
+
     def setup_graph(self,usecase):
         if usecase == "topic":
-            print("DEBUG: Condition matched! Building topic graph...")
             self.build_topic_graph()
-        else:
-            print("DEBUG: WARNING! Condition did NOT match. Skipping build!")
+        if usecase == "language":
+            self.build_language_graph()
         return self.graph.compile()
+
+llm=OpenAiLLM().get_llm()
+graph_builder=GraphBuilder(llm)
+graph = graph_builder.build_topic_graph().compile()
 
 
 
--- src/nodes/blog_node.py
+++ src/nodes/blog_node.py
@@ -1,4 +1,5 @@
-from src.states.blogstate import BlogState
+from src.states.blogstate import BlogState,Blog
+from langchain_core.messages import  SystemMessage,HumanMessage
 class BlogNode:
     def __init__(self,llm):
         self.llm= llm
@@ -22,6 +23,29 @@ class BlogNode:
             system_message=prompt.format(topic=state["topic"])
             response= self.llm.invoke(system_message)
             return {"blog":{"title":state['blog']['title'],"content":response.content}}
+
+    def translation(self, state:BlogState):
+        translation_prompt=""" Translate the following content into {current_language}.
+                    Maintain the original tone, style and formatting.
+                    Adapt cultural references and idioms to be appropriate for  {current_language}.
+                    
+                    ORIGINAL_CONTENT:{blog_content}
+        """
+        blog_content=state["blog"]["content"]
+        print(f"current_language:{state['current_language']}")
+        message=[HumanMessage(translation_prompt.format(current_language=state["current_language"],blog_content=blog_content))]
+        translation_content=self.llm.with_structured_output(Blog).invoke(message)
+        return {"blog": {"title": state['blog']['title'], "content": translation_content.content}}
+
+    def route(self,state:BlogState):
+        return {"current_language":state["current_language"]}
+
+    def route_decision(self,state:BlogState):
+        if state["current_language"] =="hindi":
+            return "hindi"
+        elif state["current_language"] =="french":
+            return "french"
+        return state["current_language"]
 
 
 
